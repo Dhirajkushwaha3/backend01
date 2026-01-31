36548ff2b4ce9880d2a6603b2d4a45c877580a46
Dhirajkushwaha3 <dherajkushwaha63@gmail.com>
2026-01-31 19:07:02 +0530

    update

--- backend/src/routes/food-partner.routes.js
+++ backend/src/routes/food-partner.routes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 const multer = require('multer');
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get('/:id', authMiddleware.authUserMiddleware, foodPartnerController.getFoodPartnerById);
+router.get('/:id',  foodPartnerController.getFoodPartnerById);
 
 // POST avatar upload (authenticated food partner only)
 router.post('/:id/avatar', authMiddleware.authFoodPartnerMiddleware, upload.single('avatar'), foodPartnerController.uploadAvatar);
